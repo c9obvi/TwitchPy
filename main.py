c2126032b4aa5e08c1c7d8e9147237e75c0293cf
Trolltsky <31329416+c9obvi@users.noreply.github.com>
2018-05-07 21:23:12 -0700

    Solved adding up session K and D
    Cleaned up the code and solved the session tracking.
    Working on last few updates next. Then, API calls for twitch chat integration with a bot

--- main.py
+++ main.py
@@ -2,21 +2,83 @@ from tkinter import *
 from tkinter import ttk
 from numpy import *
 
-#the update button funtion, including KDA calculation.
+
+#!!!!!Frame & Variables are below!!!!!
+
+root = Tk()
+#root.iconbitmap(root, default = "TpyICO.ico" )
+root.geometry('600x175')
+root.title("TwitchPy ScoreKeeper")
+myKills = StringVar()
+myDeaths = StringVar()
+myWins = StringVar()
+sessionKills = IntVar()
+kills = float
+deaths = float
+x = StringVar()
+#-----------------------------------------------------------------------------------------
+
+#-----Session Tracking Variables below -----------------------------------------
+
+seshk = []
+seshd = []
+
+#-----------LEFT-Side    INPUT/ENTRIES   &  labels----------------------------------------
+
+label_1 = Label(root, text="Wins", fg="Black").grid(row=0, sticky = E)
+label_2 = Label(root, text="Kills", fg="Black").grid(row=1, sticky = E)
+label_3 = Label(root, text="Deaths", fg="Black").grid(row=2, sticky = E)
+
+entry_1 = Entry(root, textvariable = myWins)
+entry_1.grid(row=0, column = 1)
+entry_2 = Entry(root, textvariable = myKills)
+entry_2.grid(row=1, column = 1)
+entry_3 = Entry(root, textvariable = myDeaths)
+entry_3.grid(row=2, column = 1)
+
+#---------Checkbox for auto post/delete-------------
+c = Checkbutton(root, text = "auto post for stream?")
+c.grid(columnspan=2, row=3)
+
+
+
+#---------------------------------RIGHT-Side labels------------------
+label_ws = Label(root, text = "Enter Data and Update!")
+label_ws.configure(font='Helvetica 10 bold', fg= "Darkorange")
+label_ws.grid(row=0, column=3)
+
+label_KDA = Label(root, text = "Enter K/D and Update!")
+label_KDA.grid(row=1, column=3)
+label_KDA.configure(font='Helvetica 10 bold', fg= "#73b8d2")
+
+label_killCount = Label(root, text = "Kills this session", font= 'Helvetica 10 bold', fg= "Darkgreen")
+label_killCount.grid(row=3, column=3)
+
+
+label_deathCount = Label(root, text = "Deaths this session", font= 'Helvetica 10 bold', foreground= "Red")
+label_deathCount.grid(row=4, column=3)
+
+
+#Checkbox for auto post/delete. needs function built in..
+c = Checkbutton(root, text = "auto post for stream?")
+c.grid(columnspan=2, row=3)
+
+
+#functions are listed below
 
 def wins():
     label_ws.configure(text = "Your wins Today are: " + myWins.get(), font='Helvetica 10 bold' )
 
-    kills = float(myKills.get())
-    deaths = float(myDeaths.get())
+    kills = int(myKills.get())
+    deaths = int(myDeaths.get())
 
     myKDA = kills/deaths
 
-    label_KDA.configure(text="Your KDA is: %.3f " % myKDA, font='Helvetica 10 bold')
+    label_KDA.configure(text="Your KDA that game: %.3f " % myKDA, font='Helvetica 10 bold')
 
     seshk.append(kills)
+    seshd.append(deaths)
 
-    x = str(sum(seshk))
 
     return;
 
@@ -33,7 +95,8 @@ def clearEntry():
 # attempting to make array work with sum and concatenating the sum into session stats.
 def sessionTrack():
 
-    label_killCount.configure(text= "Your kills this session are: updating here" + x.get(), font='Helvetica 10 bold')
+    label_killCount.configure(text="Your kills this session are: " + str(sum(seshk)))
+    label_deathCount.configure(text="Your deaths this session are: " + str(sum(seshd)))
 
     return;
 
@@ -47,73 +110,12 @@ def action():
 
 
 
-
-
-#-----------------------------------------------------------------------------------------
-
-#!!!!!Frame & Variables are below!!!!!
-
-root = Tk()
-#root.iconbitmap(root, default = "TpyICO.ico" )
-root.geometry('600x175')
-root.title("TwitchPy ScoreKeeper")
-myKills = StringVar()
-myDeaths = StringVar()
-myWins = StringVar()
-sessionKills = IntVar()
-
-
-#-----Session Tracking Variables below -----------------------------------------
-
-seshk = []
-
-
-#-----------LEFT-Side    INPUT/ENTRIES   &  labels----------------------------------------
-
-label_1 = Label(root, text="Wins", fg="Black").grid(row=0, sticky = E)
-label_2 = Label(root, text="Kills", fg="Black").grid(row=1, sticky = E)
-label_3 = Label(root, text="Deaths", fg="Black").grid(row=2, sticky = E)
-
-entry_1 = Entry(root, textvariable = myWins)
-entry_1.grid(row=0, column = 1)
-entry_2 = Entry(root, textvariable = myKills)
-entry_2.grid(row=1, column = 1)
-entry_3 = Entry(root, textvariable = myDeaths)
-entry_3.grid(row=2, column = 1)
-
-#---------------------------------RIGHT-Side labels------------------
-label_ws = Label(root, text = "Enter Data and Update!")
-label_ws.configure(font='Helvetica 10 bold', fg= "#ea9b2c")
-label_ws.grid(row=0, column=3)
-
-label_KDA = Label(root, text = "Enter K/D and Update!")
-label_KDA.grid(row=1, column=3)
-label_KDA.configure(font='Helvetica 10 bold', fg= "#73b8d2")
-
-label_killCount = Label(root, text = "Kills this session", font= 'Helvetica 10 bold', fg= "Green")
-label_killCount.grid(row=3, column=3)
-
-
-label_deathCount = Label(root, text = "Deaths this session", font= 'Helvetica 10 bold', foreground= "Red")
-label_deathCount.grid(row=4, column=3)
-
-
-
 #----------------------Session K & D --------------------------
 
-
-
-
-
-#Checkbox for auto post/delete
-c = Checkbutton(root, text = "auto post for stream?")
-c.grid(columnspan=2, row=3)
 
 #------------BUTTON-------------------------
 
 button_1 = ttk.Button(root, text = "Update", command= action)
 button_1.grid(row=4, column=1)
-
-
 
 root.mainloop()
